fc90454d1b2f071da16d9c1ad9954f38a2c4bfa7
viralsanghavi <vsanghavi3@gmail.com>
2023-11-11 14:54:51 +0530

    feat: updated header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,7 @@ import HeaderNavigation from "../header-navigation";
 const Header = () => {
   return (
     <nav className="px-14 py-7 pl-12 shadow-subtle fixed top-0 w-full flex justify-between items-center z-50 bg-white">
-      <h3 className="font-bold text-2xl cursor-pointer">NoErr Tech co.</h3>
+      <h3 className="font-bold text-2xl cursor-pointer">Viral Sanghavi</h3>
       <HeaderNavigation />
     </nav>
   );
